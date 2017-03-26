b3317559fae44bddf1f0f0acb93de3c552a4b73f
xiaoming <henyan94@gmail.com>
2017-03-26 16:40:25 +0200

    Started the coding of the RNN network recreated in barebone TensorFlow

---
+++ src/tensorflow-rnn.py
@@ -0,0 +1,11 @@
+'''
+===RNN Network in TensorFlow
+
+* This is a Recurrent Neural Network for sarcasm detection
+* Author: DATX02-25
+'''
+
+# Importing tensorflow
+import tensorflow as tf
+# Importing rnn framework
+from tensorflow.contrib import rnn
