f153f7c43d4de3f2022e61cd22af0b92081d5f42
Victor Risne <risne94@gmail.com>
2017-04-05 14:40:09 +0200

    Now the network is saved after the training is done

--- src/tflearn_rnn.py
+++ src/tflearn_rnn.py
@@ -65,43 +65,43 @@ class EarlyStoppingMonitor():
 				raise StopIteration
 
 def build_network(hyp):
-	net = tflearn.input_data([None, max_sequence], dtype=tf.float32) 
-	net = tflearn.embedding(net, input_dim=vocabulary_size, 
+	net = tflearn.input_data([None, max_sequence], dtype=tf.float32)
+	net = tflearn.embedding(net, input_dim=vocabulary_size,
 							     output_dim=embedding_size,
 							     name="embedding",
-							     restore=False)							 
-	
-	net = tflearn.lstm(net, 
+							     restore=False)
+
+	net = tflearn.lstm(net,
 					   64,
 					   dropout=hyp.lstm.dropout,
 					   dynamic=True,
 					   name="lstm",
 					   restore=False)
-	
+
 	"""
-	net = bidirectional_rnn(net, 
-							BasicLSTMCell(96), 
+	net = bidirectional_rnn(net,
+							BasicLSTMCell(96),
 							BasicLSTMCell(96),
 					        dynamic=True)
-	"""					        
-	net = tflearn.fully_connected(net, 
-								  64, 
+	"""
+	net = tflearn.fully_connected(net,
+								  64,
 								  activation='sigmoid',
-								  regularizer='L2', 
+								  regularizer='L2',
 								  weight_decay=hyp.middle.weight_decay,
 								  name="middle",
 								  restore=False)
 
 	net = tflearn.dropout(net, hyp.dropout.dropout, name="dropout")
-	net = tflearn.fully_connected(net, 
-								  2, 
+	net = tflearn.fully_connected(net,
+								  2,
 								  activation='softmax',
-								  regularizer='L2', 
-								  weight_decay=hyp.output.weight_decay,							  
+								  regularizer='L2',
+								  weight_decay=hyp.output.weight_decay,
 								  name="output",
 								  restore=True)
-	net = tflearn.regression(net, 
-		                     optimizer='adam', 
+	net = tflearn.regression(net,
+		                     optimizer='adam',
 		                     learning_rate=hyp.regression.learning_rate,
 	                         loss='categorical_crossentropy')
 	return net
@@ -111,13 +111,13 @@ def create_model(net, hyp, this_run_id, log_run):
 	if not (os.path.isdir(checkpoint_path)):
 		os.makedirs(checkpoint_path)
 	checkpoint_path = os.path.join("checkpoints",this_run_id + ".ckpt")
-	model = tflearn.DNN(net, 
-					    tensorboard_verbose=3, 
+	model = tflearn.DNN(net,
+					    tensorboard_verbose=3,
 					    checkpoint_path=checkpoint_path)
 
 	#set embeddings
 	if use_embeddings:
-		emb = np.array(embeddings[:vocabulary_size], dtype=np.float32)	
+		emb = np.array(embeddings[:vocabulary_size], dtype=np.float32)
 	else:
 		emb = np.random.randn(vocabulary_size, embedding_size).astype(np.float32)
 
@@ -127,31 +127,31 @@ def create_model(net, hyp, this_run_id, log_run):
 	w = model.get_weights(embeddings_tensor)
 	debug_log.log(str(w.shape), "embedding layer shape", aslist=False)
 
-	# save model
-	models_path = os.path.join("models")
-	if not (os.path.isdir(models_path)):
-		os.makedirs(models_path)
-
-	model_file_path = os.path.join(models_path,this_run_id + ".tfl")
-	#model.save(model_file_path)
-
 	return model
 
 def train_model(model, hyp, this_run_id, log_run):
 	api = EarlyStoppingMonitor(2)
 	monitorCallback = MonitorCallback(api)
 
-	model.fit(X_inputs=ps.train.xs, 
-			  Y_targets=ps.train.ys, 
-			  validation_set=(ps.valid.xs, ps.valid.ys), 
+	model.fit(X_inputs=ps.train.xs,
+			  Y_targets=ps.train.ys,
+			  validation_set=(ps.valid.xs, ps.valid.ys),
 			  show_metric=True,
-	          batch_size=batch_size, 
-	          n_epoch=epochs, 
-	          shuffle=False, 		          
+	          batch_size=batch_size,
+	          n_epoch=epochs,
+	          shuffle=False,
 	          run_id=this_run_id,
 			  snapshot_step=snapshot_steps,
 			  snapshot_epoch=True,
 	          callbacks=monitorCallback)
+
+	# save model
+	models_path = os.path.join("models")
+	if not (os.path.isdir(models_path)):
+		os.makedirs(models_path)
+
+	model_file_path = os.path.join(models_path,this_run_id + ".tfl")
+	model.save(model_file_path)
 
 	return model
 
@@ -182,7 +182,7 @@ def do_prediction(model, hyp, this_run_id, log_run):
 
 ################################################################################
 
-# Handles command arguments, usefull for debugging 
+# Handles command arguments, usefull for debugging
 arghandler = Arg_handler(sys.argv[1:])
 # usage: tflearn_rnn.py --pfn debug_processed.pickle
 #  will get samples from debug_processed.pickle
@@ -211,12 +211,12 @@ with open(samples_path, 'rb') as handle:
     ps = pickle.load( handle )
 
 # debug print tweets
-if print_debug:	
+if print_debug:
 	for s_id, s_y, s_x in zip(ps.train.ids, ps.train.ys, ps.train.xs):
 		ispos = np.array_equal(s_y, pos_label)
 		label = "Positive (Sarcastic)" if ispos else "Negative (not sarcastic)"
 		logstring = "Sample id: {}, {}: {:<5}".format(s_id, label, "\n")
-		logstring += " ".join( reverse_lookup(s_x, rev_vocabulary, ascii_console )) 
+		logstring += " ".join( reverse_lookup(s_x, rev_vocabulary, ascii_console ))
 		debug_log.log(logstring, logname="reverse_lookup", maxlogs = 5, step = 2500)
 	print("\nLogged sample values:\n")
 	debug_log.print_log(logname="reverse_lookup")
@@ -224,7 +224,7 @@ if print_debug:
 
 
 # 'sönderhaxad' class that generates random hyperparamters in the range provided
-hypers = Hyper(run_count, 
+hypers = Hyper(run_count,
 	lstm = {'dropout': (0.4, 0.8)},
 	middle= {'weight_decay': (0.01, 0.03)},
 	dropout = {'dropout': (0.4, 0.8)},
@@ -238,8 +238,8 @@ for hyp in hypers:
 	this_run_id = common_funs.generate_name()
 	log_run.log(hyp.get_hypers(), logname='hypers', aslist = False)
 	log_run.log(this_run_id, logname='run_id', aslist = False)
-	
-	tf.reset_default_graph()              
+
+	tf.reset_default_graph()
 	with tf.Graph().as_default(), tf.Session() as sess:
 		sess.run(tf.initialize_all_variables())
 		tflearn.config.init_training_mode()
@@ -252,12 +252,3 @@ for hyp in hypers:
 	perflog.save("training_performance.csv", append = True)
 
 debug_log.save("training_debug.log")
-
-
-
-
-
-
-
-
-
