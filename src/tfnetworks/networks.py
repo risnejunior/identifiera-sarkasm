62de0879a1637bf32399e710ac8cdfeb079a918c
xiaoming <henyan94@gmail.com>
2017-04-21 17:03:42 +0200

    added a fetch_network method for fetching neural networks by name

---
+++ src/tfnetworks/networks.py
@@ -0,0 +1,15 @@
+"""
+This is the file containing the abstract class for the neural networks.
+It also contains method for getting a specific network that is inheriting
+of the abstract class
+"""
+
+class abstract_network(object):
+    def feed_network(data,keep_prob):
+        raise NotImplementedError("This method is not implemented")
+
+    def calc_l2_loss():
+        raise NotImplementedError("This method is not implemented")
+
+def fetch_network(name=None):
+    return None
