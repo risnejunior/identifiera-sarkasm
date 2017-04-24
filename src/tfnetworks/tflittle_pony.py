406b30cb0c09f600a356d04003d3d27356a62107
xiaoming <henyan94@gmail.com>
2017-04-24 20:04:28 +0200

    Added Dropout to the RNN GRU cell in little pony

--- src/tfnetworks/tflittle_pony.py
+++ src/tfnetworks/tflittle_pony.py
@@ -5,7 +5,7 @@ from .networks import AbstractNetwork
 class LittlePonyNetwork(AbstractNetwork):
     def __init__(self,n_classes,rnn_size = 256):
         self._name = "little pony"
-        self._layer_weights = tf.Variable(tf.random_uniform([rnn_size,n_classes]), name="Weights")
+        self._layer_weights = tf.Variable(tf.random_uniform([rnn_size,n_classes]), name="weights")
         self._layer_biases = tf.Variable(tf.random_uniform([n_classes]), name="biases")
         self._GRU_cell = rnn.GRUCell(rnn_size)
 
@@ -13,10 +13,11 @@ class LittlePonyNetwork(AbstractNetwork):
         dimensions = data.get_shape().as_list()
         batch_size = dimensions[0]
         weight_dropout = tf.nn.dropout(self._layer_weights, keep_prob)
+        rnn_dropout = rnn.core_rnn_cell.DropoutWrapper(self._GRU_cell,output_keep_prob=keep_prob)
         data = tf.transpose(data,[1,0,2])
         data = tf.reshape(data,[-1,chunk_size])
         sequence = tf.split(data, n_chunks, 0)
-        outputs, states = rnn.static_rnn(self._GRU_cell, sequence, dtype=tf.float32)
+        outputs, states = rnn.static_rnn(rnn_dropout, sequence, dtype=tf.float32)
         output = tf.add(tf.matmul(outputs[-1],weight_dropout), self._layer_biases)
         return output
 
