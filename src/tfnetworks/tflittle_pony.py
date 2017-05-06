82d2d539b2d95aedf6c55ee79104791dc0fee393
xiaoming <henyan94@gmail.com>
2017-05-06 17:48:27 +0200

    Added tflearn recurrent network dynamic=True like features in tensorflow rnn

--- src/tfnetworks/tflittle_pony.py
+++ src/tfnetworks/tflittle_pony.py
@@ -1,24 +1,27 @@
 import tensorflow as tf
 from tensorflow.contrib import rnn
-from .networks import AbstractNetwork
+from . import networks as net
 
-class LittlePonyNetwork(AbstractNetwork):
+class LittlePonyNetwork(net.AbstractNetwork):
     def __init__(self,n_classes,rnn_size = 256):
         self._name = "little_pony"
         self._layer_weights = tf.Variable(tf.random_uniform([rnn_size,n_classes]), name="weights")
         self._layer_biases = tf.Variable(tf.random_uniform([n_classes]), name="biases")
-        self._GRU_cell = rnn.GRUCell(rnn_size)
+        self._lstm_cell = rnn.BasicLSTMCell(rnn_size)
 
     def feed_network(self,data,keep_prob,chunk_size,n_chunks):
+        sequence_lengths = net.calc_seqlenth(data)
         dimensions = data.get_shape().as_list()
         batch_size = dimensions[0]
         weight_dropout = tf.nn.dropout(self._layer_weights, keep_prob)
-        rnn_dropout = rnn.core_rnn_cell.DropoutWrapper(self._GRU_cell,output_keep_prob=keep_prob)
+        rnn_dropout = rnn.core_rnn_cell.DropoutWrapper(self._lstm_cell,output_keep_prob=keep_prob)
         data = tf.transpose(data,[1,0,2])
         data = tf.reshape(data,[-1,chunk_size])
         sequence = tf.split(data, n_chunks, 0)
-        outputs, states = rnn.static_rnn(rnn_dropout, sequence, dtype=tf.float32)
-        output = tf.add(tf.matmul(outputs[-1],weight_dropout), self._layer_biases)
+        outputs, states = rnn.static_rnn(rnn_dropout, sequence, dtype=tf.float32, sequence_length = sequence_lengths)
+        outputs = tf.transpose(tf.stack(outputs), [1, 0, 2])
+        output = net.advanced_indexing_op(outputs, sequence_lengths)
+        output = tf.add(tf.matmul(output,weight_dropout), self._layer_biases)
         return output
 
     def get_name(self):
