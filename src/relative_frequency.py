7d612f2385680b74a8f58ecf32fcb6eb533edf45
Jan Pettersson <mail@janpettersson>
2017-05-10 13:55:14 +0200

    switched the freq baseline to new model, added support for ratio calc

--- src/relative_frequency.py
+++ src/relative_frequency.py
@@ -3,6 +3,7 @@ import numpy as np
 import pickle
 from collections import Counter
 import random
+from scipy.stats import gmean
 
 from common_funs import Progress_bar
 from common_funs import Binary_confusion_matrix
@@ -23,11 +24,14 @@ def _arg_callback_ds(ds_name):
 	cfg.dataset_name = ds_name
 	print("<Using dataset: {}>".format(ds_name))
 
-def relative_frequency(ns, nu): 
-	""" returns -1 to +1, where +1 is most sarcastic"""
-	return ((ns - nu) / (ns + nu))
+def relative_frequency(ns, nu, ratio): 
+	""" 
+		returns -1 to +1, where +1 is most sarcastic, ratio is used to balance- 
+		  out unbalanced datasets.
+	"""
+	return ((ns*ratio - nu) / (ns*ratio + nu))
 
-def predict(samples):
+def predict(samples, frequencies):
 	predictions = []
 	pb = Progress_bar(len(samples)-1)
 	for i, words in enumerate(samples):
@@ -52,13 +56,13 @@ arghandler = Arg_handler()
 arghandler.register_flag('ds', _arg_callback_ds, ['select-dataset', 'dataset'], "Which dataset to use. Args: <dataset-name>")
 arghandler.register_flag('in', _arg_callback_in, ['input', 'in-file'], "Which file to take samples from. args: <filename>")
 arghandler.consume_flags()
-print("-----------------------------------------------------------------------")
+print("-" * 70)
 
 # load processed samples
 with open(cfg.samples_path, 'rb') as handle:
     ps = (pickle.load(handle)).dataset
 
-# separate words into positive & negative
+# build list of words contained in samples from each class 
 print("Separating words into each class..")
 pos_words = []; neg_words = [];
 for words, y in  zip(ps.train.xs, ps.train.ys):
@@ -67,10 +71,12 @@ for words, y in  zip(ps.train.xs, ps.train.ys):
 	else:
 		neg_words.extend(words)
 	
-# count words
+# count words in each class
 print("Counting word occurrences for each class..")
 pos_counts = Counter(pos_words)
 neg_counts = Counter(neg_words)
+ratio = len(neg_words) / len(pos_words)
+print("Ratio between neutral and positive words: {}".format(ratio))
 
 # make set of all words
 all_words = set()
@@ -81,16 +87,16 @@ for word in list(pos_counts.keys()) + list(neg_counts.keys()):
 print("Calculating relative frequency for every words..")
 frequencies = {}
 for i, word in enumerate(all_words):
-	frequencies[word] = relative_frequency(pos_counts[word], neg_counts[word])
+	frequencies[word] = relative_frequency(pos_counts[word], neg_counts[word], ratio)
 
 # classify samples and print confusion matrix
 print("Classifying samples..\n")
 cm = Binary_confusion_matrix()
 
-predictions = predict(ps.train.xs)
+predictions = predict(ps.train.xs, frequencies)
 cm.calc(ps.train.ids , predictions, ps.train.ys, 'training-set')
 
-predictions = predict(ps.valid.xs)
+predictions = predict(ps.valid.xs, frequencies)
 cm.calc(ps.valid.ids , predictions, ps.valid.ys, 'validation-set')
 
 cm.print_tables()
